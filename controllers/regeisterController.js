e27dd2797f0fd24637f661ea32486a99beed7f2f
mehdi <mehdi.hakmoune@gmail.com>
2024-03-01 20:51:32 +0000

    MongoDB Data Models

--- controllers/regeisterController.js
+++ controllers/regeisterController.js
@@ -1,12 +1,4 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function(data) {
-    this.users = data;
-  }
-};
-
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 // Register a New user
@@ -20,26 +12,20 @@ const handleNewUser = async (req, res) => {
     });
 
   // check for dublicate usernames in the database
-  const duplicate = usersDB.users.find(persone => persone.username === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) return res.sendStatus(409); // 409 Conflict
 
   try {
     // encrypt the pwd
     const hashedPwd = await bcrypt.hash(pwd, 10);
 
-    // store the new user
-    const newUser = {
+    // Create and Store the new user
+    const result = await User.create({
       username: user,
-      password: hashedPwd,
-      roles: { user: 2001 }
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
+      password: hashedPwd
+    });
+    console.log(result);
 
-    console.log(usersDB.users);
     res.status(201).json({ success: `New User ${user} created !` });
   } catch (err) {
     res.status(500).json({ message: err.message });
---
+++ model/Employee.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Chaque schéma correspond à une collection MongoDB
+const employeeSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  }
+});
+
+// Une instance d'un modèle est appelée un document
+// Le premier argument "Employee" est le nom singulier de la collection à laquelle votre modèle est destiné.
+// Mongoose recherche automatiquement la version plurielle et minuscule du nom de votre modèle.
+// le "model" "Employee" est destiné à la collection "employees" dans la base de données.
+module.exports = mongoose.model("Employee", employeeSchema);
